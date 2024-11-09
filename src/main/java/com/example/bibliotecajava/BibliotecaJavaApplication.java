944ddc8e22d085c8602d2e9de5d2dcaee732b85e
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-11-09 17:35:34 -0300

    primeiro commit

---
+++ src/main/java/com/example/bibliotecajava/BibliotecaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.bibliotecajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bibliotecajava/BibliotecaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bibliotecajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
